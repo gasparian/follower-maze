5cbb0cdca18bf25eff863bc1ef7206ce12053729
gas <gasparian>
2021-12-26 03:25:20 +0500

    heap and clients chans implemented; NTT

--- Makefile
+++ Makefile
@@ -1,15 +1,37 @@
 TEST=go test -v -cover -race -count=1 -timeout 30s $(1)
-.DEFAULT_GOAL := build
+DEFAULT_GOAL := help
 
 .SILENT:
-.PHONY: build
+.PHONY: help, build, build-static, run-simulator, test
+
+help:
+	echo
+	echo '  Usage:'
+	echo '    make <target>'
+	echo
+	echo '  Targets:'
+	echo '    build          generate binary'
+	echo '    build          generate statically linked binary'
+	echo '    simulate       run events and clients simulator'
+	echo '    simulate-test  run simulator configured just to test app functionality'
+	echo '    test           run tests'
+
 build:
 	go build -v ./cmd/server
 
 build-static:
-	CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -v -a ./cmd/server
+	CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build \
+		-ldflags "-w -extldflags -static" \
+		-tags osusergo,netgo \
+		-v -a ./cmd/server
 
-run-simulator:
+simulate:
+	./simulator/followermaze.sh
+
+simulate-test:
+	totalEvents=100 concurrencyLevel=2 \
+	numberOfUsers=2 timeout=20000 \
+	maxEventSourceBatchSize=2 \
 	./simulator/followermaze.sh
 
 .PHONY: test
--- cmd/server/server.go
+++ cmd/server/server.go
@@ -12,6 +12,8 @@ import (
 	// "encoding/binary"
 	"time"
 	// "unicode/utf8"
+	"container/heap"
+	"sync"
 )
 
 const (
@@ -20,6 +22,11 @@ const (
 	Broadcast    = 66
 	PrivateMsg   = 80
 	StatusUpdate = 83
+)
+
+var (
+	clients     = InitClientsChans(10)
+	eventsQueue = InitPriorityQueue()
 )
 
 type Event struct {
@@ -31,12 +38,135 @@ type Event struct {
 
 type handler func(conn net.Conn)
 
-type ClientsChans map[uint64]chan *Event
+type ClientsChans struct {
+	mx         sync.RWMutex
+	items      map[uint64]chan *Event
+	bufferSize uint
+}
+
+func InitClientsChans(bufferSize uint) *ClientsChans {
+	return &ClientsChans{
+		items:      make(map[uint64]chan *Event),
+		bufferSize: bufferSize,
+	}
+}
+
+func (c *ClientsChans) InitChan(id uint64) chan *Event {
+	c.mx.Lock()
+	defer c.mx.Unlock()
+	ch := make(chan *Event, c.bufferSize)
+	c.items[id] = ch
+	return ch
+}
+
+func (c *ClientsChans) GetChan(id uint64) chan *Event {
+	c.mx.RLock()
+	defer c.mx.RUnlock()
+	return c.items[id]
+}
+
+func (c *ClientsChans) Send(id uint64, event *Event) {
+	ch := c.GetChan(id)
+	ch <- event
+}
+
+func (c *ClientsChans) Size() int {
+	return len(c.items)
+}
+
+type EventsMinHeap []*Event
+
+func (h EventsMinHeap) Len() int {
+	return len(h)
+}
+
+func (h EventsMinHeap) Less(i, j int) bool {
+	return h[i].Number < h[j].Number
+}
+
+func (h EventsMinHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+}
+
+func (h *EventsMinHeap) Push(x interface{}) {
+	*h = append(*h, x.(*Event))
+}
+
+func (h *EventsMinHeap) Pop() interface{} {
+	old := *h
+	tailIndex := old.Len() - 1
+	tail := old[tailIndex]
+	old[tailIndex] = nil
+	*h = old[:tailIndex]
+	return tail
+}
+
+type PriorityQueue struct {
+	mx     sync.RWMutex
+	events *EventsMinHeap
+}
+
+func InitPriorityQueue() *PriorityQueue {
+	return &PriorityQueue{
+		events: new(EventsMinHeap),
+	}
+}
+
+func (p *PriorityQueue) Push(event *Event) {
+	p.mx.Lock()
+	defer p.mx.Unlock()
+	heap.Push(
+		p.events,
+		event,
+	)
+}
+
+func (p *PriorityQueue) Pop() *Event {
+	p.mx.Lock()
+	defer p.mx.Unlock()
+	return heap.Pop(p.events).(*Event)
+}
+
+func (p *PriorityQueue) Size() int {
+	p.mx.RLock()
+	defer p.mx.RUnlock()
+	return p.events.Len()
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func parsePayload(req string) (*Event, error) {
+	var fromUserID, toUserID uint64
+	var parsed []string = strings.Split(req, "|")
+	number, err := strconv.ParseUint(parsed[0], 10, 64)
+	if err != nil {
+		return nil, err
+	}
+	if len(parsed) == 3 {
+		fromUserID, err = strconv.ParseUint(parsed[2], 10, 64)
+	}
+	if len(parsed) == 4 {
+		toUserID, err = strconv.ParseUint(parsed[3], 10, 64)
+	}
+	if err != nil {
+		return nil, err
+	}
+	return &Event{
+		Number:     number,
+		MsgType:    int(parsed[1][0]),
+		FromUserID: fromUserID,
+		ToUserID:   toUserID,
+	}, nil
+}
 
 func handleEvent(conn net.Conn) {
 	conn.SetReadDeadline(time.Now().Add(20000 * time.Millisecond))
 	request := make([]byte, 128)
-	var fromUserID, toUserID uint64
 	defer conn.Close()
 	for {
 		read_len, err := conn.Read(request)
@@ -48,20 +178,9 @@ func handleEvent(conn net.Conn) {
 
 		req := strings.Fields(string(request[:read_len]))
 		for _, r := range req {
-			parsed := strings.Split(r, "|")
-			number, _ := strconv.ParseUint(parsed[0], 10, 64)
-			if len(parsed) > 2 {
-				fromUserID, _ = strconv.ParseUint(parsed[2], 10, 64)
-			}
-			if len(parsed) > 3 {
-				toUserID, _ = strconv.ParseUint(parsed[3], 10, 64)
-			}
-			log.Println(Event{
-				Number:     number,
-				MsgType:    int(parsed[1][0]),
-				FromUserID: fromUserID,
-				ToUserID:   toUserID,
-			})
+			event, _ := parsePayload(r)
+			eventsQueue.Push(event)
+			log.Printf("%s -- %v -- %v \n", r, event, eventsQueue.Size())
 		}
 		log.Println()
 	}
@@ -86,14 +205,8 @@ func handleClient(conn net.Conn) {
 			log.Println(err)
 			continue
 		}
-		log.Println("New client connected: ", clientId)
-	}
-}
-
-func checkError(err error) {
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
-		os.Exit(1)
+		clients.InitChan(clientId)
+		log.Println("New client connected: ", clientId, " ", clients.Size())
 	}
 }
 
@@ -113,7 +226,7 @@ func StartTCPServer(service string, handlerFunc handler) {
 }
 
 func main() {
-	log.SetOutput(os.Stdout) // log writes to stderr by default
+	log.SetOutput(os.Stdout) // force log to stdout
 	runtime.GOMAXPROCS(2)
 
 	go StartTCPServer(":9099", handleClient)
